7732b1605139376ce5dcef3b52aff1713bb83c37
Vasinov Aleksej <proglamer@gmail.com>
2016-03-17 14:22:25 +0200

    Add version to layouts
    Static, without link to Git

---
+++ GiftService.Web/WebsiteVersion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiftService.Web
+{
+    public class WebsiteVersion
+    {
+        public static string Version
+        {
+            get
+            {
+                return "v1.1.0";
+            }
+        }
+    }
+}
