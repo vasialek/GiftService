3f806f60dd0f36130fda689da67f6c6c66098e55
Vasinov Aleksej <proglamer@gmail.com>
2016-08-11 13:32:50 +0300

    Gift controller is checked in

--- GiftService.Web/Controllers/GiftController.cs
+++ GiftService.Web/Controllers/GiftController.cs
@@ -1,5 +1,6 @@
 ﻿using GiftService.Models;
 using GiftService.Models.Exceptions;
+using GiftService.Web.Models.Gifts;
 using log4net;
 using System;
 using System.Collections.Generic;
@@ -89,16 +90,110 @@ namespace GiftService.Web.Controllers
         // GET: /Gift/Download/UniqueGiftId
         public ActionResult Download(string id)
         {
-            return OutputFile(id, true);
+            return OutputFile(id, false, true);
         }
 
         // GET: /Gift/Print/UniqueGiftId
         public ActionResult Print(string id)
         {
-            return OutputFile(id, false);
+            return OutputFile(id, false, false);
         }
 
-        private ActionResult OutputFile(string productUid, bool forceDownload)
+        // POST: /Gift/Friend
+        [HttpPost]
+        public ActionResult Friend(/*Models.Gifts.GiftToFriendModel model*/FormCollection form)
+        {
+            Logger.Info("Preparing gift coupon for friend...");
+            var model = new GiftToFriendModel();
+            TryUpdateModel<GiftToFriendModel>(model, "friendEmail");
+            Logger.DebugFormat("  friend e-mail:   `{0}`", model.FriendEmail);
+            Logger.DebugFormat("  text for friend: `{0}`", model.Text);
+            Logger.DebugFormat("  BtnDownloadGift: `{0}`", model.BtnDownloadGift);
+            Logger.DebugFormat("  BtnEmailGift:    `{0}`", model.BtnEmailGift);
+
+            try
+            {
+                Factory.GiftsBll.MakeCouponGift(model.ProductUid, model.FriendEmail, model.Text);
+            }
+            catch (InvalidProductException ipex)
+            {
+                string msg;
+                switch (ipex.Reason)
+                {
+                    case InvalidProductException.Reasons.ProductExpired:
+                        msg = ipex.Message;
+                        break;
+                    case InvalidProductException.Reasons.ProductIsGiftAlready:
+                        msg = "This coupon was dedicated to other user";
+                        break;
+                    default:
+                        msg = Resources.Language.SystemMessage_WeWillContactYouOnError;
+                        break;
+                }
+                this.SetTempMessage(msg);
+                return Error();
+            }
+
+            return OutputFile(model.ProductUid, true, true);
+        }
+
+        // POST: /Gift/EmailFriend
+        public ActionResult EmailFriend(FormCollection form)
+        {
+            bool isOk = false;
+            string msg = "";
+
+            Logger.Info("Preparing gift coupon for friend...");
+            var model = new GiftToFriendModel();
+            TryUpdateModel<GiftToFriendModel>(model, "friendEmail");
+            Logger.DebugFormat("  friend e-mail:   `{0}`", model.FriendEmail);
+            Logger.DebugFormat("  text for friend: `{0}`", model.Text);
+            Logger.DebugFormat("  BtnDownloadGift: `{0}`", model.BtnDownloadGift);
+            Logger.DebugFormat("  BtnEmailGift:    `{0}`", model.BtnEmailGift);
+
+            try
+            {
+                Factory.GiftsBll.MakeCouponGift(model.ProductUid, model.FriendEmail, model.Text);
+            }
+            catch (InvalidProductException ipex)
+            {
+                switch (ipex.Reason)
+                {
+                    case InvalidProductException.Reasons.ProductExpired:
+                        msg = ipex.Message;
+                        break;
+                    case InvalidProductException.Reasons.ProductIsGiftAlready:
+                        msg = "This coupon was dedicated to other user";
+                        break;
+                    default:
+                        msg = Resources.Language.SystemMessage_WeWillContactYouOnError;
+                        break;
+                }
+            }
+
+            try
+            {
+                var pdf = Factory.PdfBll.GeneratProductPdf(model.ProductUid, true);
+                var product = Factory.GiftsBll.GetProductInformationByUid(model.ProductUid);
+                Factory.CommunicationBll.SendCouponAsGift(model.FriendEmail, pdf, product);
+
+                isOk = true;
+                msg = String.Concat("PDF coupon is successfully sent to your friend email ", model.FriendEmail);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Error sending PDF coupon as gift", ex);
+                msg = ex.Message;
+            }
+
+            return Json(new
+            {
+                Status = isOk,
+                Message = msg
+            });
+        }
+
+        private ActionResult OutputFile(string productUid, bool asGift, bool forceDownload)
         {
             if (String.IsNullOrEmpty(productUid))
             {
@@ -108,7 +203,7 @@ namespace GiftService.Web.Controllers
             try
             {
                 //byte[] pdf = Factory.PdfBll.GetProductPdf(productUid);
-                byte[] pdf = Factory.PdfBll.GeneratProductPdf(productUid);
+                byte[] pdf = Factory.PdfBll.GeneratProductPdf(productUid, asGift);
 
                 if (forceDownload)
                 {
@@ -134,6 +229,12 @@ namespace GiftService.Web.Controllers
         public ViewResult NotFound()
         {
             return View("NotFound");
+        }
+
+        // GET: /Gift/Error
+        public ViewResult Error()
+        {
+            return View("Error");
         }
 
     }
---
+++ doc/database/ChangeScripts/1.4/product_add_for_gift.sql
@@ -0,0 +1,6 @@
+alter table product add gift_email varchar(256) null;
+
+alter table product add gift_text nvarchar(512) null;
+
+-- Do not use
+-- alter table product add gift_uid varchar(32) null;
